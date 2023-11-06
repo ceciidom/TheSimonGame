efa3c3fedd65798005583c2f9d7c38bf72014aeb
Cecilia Dominguez <ceciidom98@gmail.com>
2023-11-05 23:34:33 -0600

    Now I added the new versions of game and index

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -7,21 +7,31 @@ var gamePattern = [];
 //3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
 var buttonColours = ["red","blue","green","yellow"];
 
+//Step 7 - Start the game
+var started = false;
+var level = 0;
 
+$(document).keypress(function () {
+  if (!started) {
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 function nextSequence(){
-   var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
 
-   gamePattern.push(randomChosenColour);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
 
-  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
- 
-    playSound(randomChosenColour);
+  gamePattern.push(randomChosenColour);
 
-    //first key
-    $("h1").text("Level 0");
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
 
+  playSound(randomChosenColour);
 }
 
 $(".btn").click(function(userChosenColour){
@@ -45,8 +55,7 @@ function animatePress (currentColour) {
     setTimeout(function(){$("#" + currentColour).removeClass("pressed")}, 100);
     }
 
-//Step 7 - Start the game
-$("body").one("keypress", nextSequence())
+
 
 
 
